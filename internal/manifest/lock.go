4089826a2e94b2c9caaeafdbe194b78c4ffe8e05
James Johns <192176108+jamesjohnsdev@users.noreply.github.com>
2026-08-22 16:20:21 +1000

    feat: lockfile creation and parsing

---
+++ internal/manifest/lock.go
@@ -0,0 +1,40 @@
+package manifest
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type LockFile struct {
+	Entries map[string]LockEntry
+}
+
+type LockEntry struct {
+	Version string `toml:"version"`
+	Hash    string `toml:"hash"`
+}
+
+func ParseLock(path string) (*LockFile, error) {
+	// create blank Lock entry map with string
+	entries := make(map[string]LockEntry)
+	if _, err := toml.DecodeFile(path, &entries); err != nil {
+		// if no lock file, treat as if blank
+		if errors.Is(err, fs.ErrNotExist) {
+			return &LockFile{Entries: entries}, nil
+		}
+		return nil, err
+	}
+	return &LockFile{Entries: entries}, nil
+}
+
+func WriteLock(path string, lf *LockFile) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return toml.NewEncoder(file).Encode(lf.Entries)
+}
