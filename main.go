01444e2492b651cb0787b024ccbb5a65f8d9ee2a
James Johns <192176108+jamesjohnsdev@users.noreply.github.com>
2026-08-25 16:51:10 +1000

    feat: config override support
    Config package is initialised in `main.go` and allows user override of
    default configuration via config file or env variables.

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/alecthomas/kong"
 	"github.com/jamesjohnsdev/bag/internal/cmd"
+	"github.com/jamesjohnsdev/bag/internal/config"
 )
 
 func main() {
+	config.Load()
 	ctx := kong.Parse(&cmd.CLI{})
 	ctx.FatalIfErrorf(ctx.Run())
 }
