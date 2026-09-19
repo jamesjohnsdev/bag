5b18bf3883779e6b433aff83ef574cf87cde5eae
James Johns <192176108+jamesjohnsdev@users.noreply.github.com>
2026-09-20 00:21:06 +1000

    feat: manifest commands
    manifest view, list, check commands
    also same commands with `tool`

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -15,14 +15,15 @@ type CLI struct {
 
 	ManInstall ManInstallCmd `cmd:"" name:"man-install" help:"Install man page for local use"`
 
-	Init   InitCmd   `cmd:"" help:"Initialise a bag"`
-	Add    AddCmd    `cmd:"" help:"Add a binary"`
-	Update UpdateCmd `cmd:"" help:"Update a binary"`
-	List   ListCmd   `cmd:"" help:"List all stored versions of a binary"`
-	Remove RemoveCmd `cmd:"" help:"Remove an installed binary"`
-	View   ViewCmd   `cmd:"" help:"View details of an installed binary"`
-	Which  WhichCmd  `cmd:"" help:"Find the stored path of a binary"`
-	Tool   ToolCmd   `cmd:"" help:"Manage project tools"`
+	Init     InitCmd     `cmd:"" help:"Initialise a bag"`
+	Add      AddCmd      `cmd:"" help:"Add a binary"`
+	Update   UpdateCmd   `cmd:"" help:"Update a binary"`
+	List     ListCmd     `cmd:"" help:"List all stored versions of a binary"`
+	Manifest ManifestCmd `cmd:"" help:"Interact with the manifest"`
+	Remove   RemoveCmd   `cmd:"" help:"Remove an installed binary"`
+	View     ViewCmd     `cmd:"" help:"View details of an installed binary"`
+	Which    WhichCmd    `cmd:"" help:"Find the stored path of a binary"`
+	Tool     ToolCmd     `cmd:"" help:"Manage project tools"`
 }
 
 // TODO: Consider changing definitions for commented out ones
